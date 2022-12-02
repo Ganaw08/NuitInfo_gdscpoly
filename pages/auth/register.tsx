a9a411a981c69a76d7d5266b205e69393c3aa86c
imac <imac@Mahdi-iMac.local>
2022-12-02 02:36:30 +0100

    register

---
+++ pages/auth/register.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @next/next/no-sync-scripts */
+/* eslint-disable react/jsx-no-comment-textnodes */
+export default function Register(): JSX.Element {
+ return (
+     <><div className="container">
+         <div className="form_area">
+             <h4 className="title">Register Now</h4>
+             <form>
+                 <div className="form_group">
+                     <label htmlFor="email" className="sub_title">Email</label>
+                     <input type="email" id="email" className="form_style" />
+                 </div>
+                 <div className="form_group">
+                     <label htmlFor="password" className="sub_title">Password</label>
+                     <input type="password" id="password" className=" form_style" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
+                 </div>
+                 <div>
+                     <button className="btn">Register</button>
+                 </div>
+             </form>
+         </div>
+     </div><div id="message">
+             <h3>Password must contain the following:</h3>
+             <p id="letter" className="invalid">A <b>lowercase</b> letter</p>
+             <p id="capital" className="invalid">A <b>capital (uppercase)</b> letter</p>
+             <p id="number" className="invalid">A <b>number</b></p>
+             <p id="length" className="invalid">Minimum <b>8 characters</b></p>
+         // eslint-disable @next/next/no-sync-scripts, @next/next/no-sync-scripts
+         </div><script src="register.js"></script></>
+          )}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { GetStaticProps, NextPage } from "next";
 import { useRef, useState, Fragment } from "react";
 import { useRouter } from "next/router";
-
+import Register from "./auth/register";
 import Link from "next/link";
 import Layout from "../components/layout";
 import Header from "../components/header";
 import Footer from "../components/footer";
+
 
 const Home: NextPage<{}> = () => {
 
@@ -16,6 +17,7 @@ const Home: NextPage<{}> = () => {
           I am the Home Page
         </div>
       </Layout>
+      {/* <Register/>  */}
     </>
   );
 };
